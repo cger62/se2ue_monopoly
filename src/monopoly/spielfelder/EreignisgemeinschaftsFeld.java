59463bef4999f0284303cd83b7210c8dff6927a2
SaliHassan <s61736@beuth-hochschule.de>
2017-01-10 13:37:35 +0100

    Update EreignisgemeinschaftsFeld

--- src/monopoly/spielfelder/EreignisgemeinschaftsFeld.java
+++ src/monopoly/spielfelder/EreignisgemeinschaftsFeld.java
@@ -9,44 +9,37 @@ import java.util.*;
  */
 public class EreignisgemeinschaftsFeld implements Spielfelder {
 
-    /**
-     * Default constructor
-     */
-    public EreignisgemeinschaftsFeld() {
-    }
-
-    /**
-     * 
-     */
-    public int feldnummer;
-
-    /**
-     * 
-     */
-    public String feldname;
-
-
-
-
-    /**
-     * 
-     */
-    public EreignisgemeinschaftsKarte 16;
+    private final int feldnummer;
+    private final String feldname;
+    public EreignisgemeinschaftsKarte[] karten = {new EreignisgemeinschaftsKarte(50, "Schulgeld", true),
+                                                  new EreignisgemeinschaftsKarte(20, "Geburtstag", false),
+                                                  new EreignisgemeinschaftsKarte(80, "Krankenhaus", true),
+                                                  new EreignisgemeinschaftsKarte(15, "Strafzettel", true),
+                                                  new EreignisgemeinschaftsKarte(50, "Urlaubsgeld", false),
+                                                  new EreignisgemeinschaftsKarte(100, "Versicherung", true),
+                                                  new EreignisgemeinschaftsKarte(10, "Schönheitswettbewerb", false),
+                                                  new EreignisgemeinschaftsKarte(50, "Wette", false),
+                                                  new EreignisgemeinschaftsKarte(200, "Los", false),
+                                                  new EreignisgemeinschaftsKarte(0, "Gefängnis", false),
+                                                  new EreignisgemeinschaftsKarte(0, "Südbahnhof", false),
+                                                  new EreignisgemeinschaftsKarte(0, "Opernplatz", false),
+                                                  new EreignisgemeinschaftsKarte(0, "Schlossallee", false),
+                                                  new EreignisgemeinschaftsKarte(0, "Seestraße", false),
+                                                  new EreignisgemeinschaftsKarte(0, "3zurück", false),
+                                                  new EreignisgemeinschaftsKarte(0, "3vor", false)};
 
     /**
      * @param feldnummer 
      * @param feldname
      */
-    public void EreignisgemeinschaftsFeld(int feldnummer, String feldname) {
-        // TODO implement here
+    public EreignisgemeinschaftsFeld(int feldnummer, String feldname) {
+        this.feldnummer = feldnummer;
+        this.feldname = feldname;        
     }
-
-    /**
-     * @return
-     */
+    
     public EreignisgemeinschaftsKarte randomKarte() {
-        // TODO implement here
-        return null;
+        int random = new Random().nextInt(karten.length);
+        EreignisgemeinschaftsKarte result = (karten[random]); 
+        return result;
     }
-
 }
